6db2950f4381eb8ed457f038c2b82954790a312b
hoangan-mi <lehangnga2279@gmail.com>
2026-03-08 14:56:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -308,7 +308,7 @@ def api_asset(asset_id):
 # =========================
 # UPDATE LOCATION
 # =========================
-@app.route("/update_location", methods=["POST"])
+@app.route("/update-location", methods=["POST"])
 @require_role(["admin","manager"])
 def update_location():
 
@@ -504,6 +504,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
 
     app.run(host="0.0.0.0", port=port, debug=True) 
+
 
 
 
